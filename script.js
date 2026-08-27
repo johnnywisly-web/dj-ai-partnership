627c582ca0449cf0f71333645cc70828c318a6f5
johnnywisly-web <johnnywisly@gmail.com>
2026-08-27 05:17:50 -0400

    Add 'Apply Now' link to script.js
    <a href="https://forms.gle/seTs2W87QmFvXMPX8" target="_blank" rel="noopener">
        Apply Now
    </a>

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-// Applications are handled by the linked Google Form.
+<a href="https://forms.gle/seTs2W87QmFvXMPX8" target="_blank" rel="noopener">
+    Apply Now
+</a>// Applications are handled by the linked Google Form.
