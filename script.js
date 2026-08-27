f63a423a94f70519e20b165fef63f19e690edf78
johnnywisly-web <johnnywisly@gmail.com>
2026-08-27 05:40:31 -0400

    Add files via upload
    <a href="https://forms.gle/seTs2W87QmFvXMPX8" target="_blank" rel="noopener">
        Apply Now
    </a>

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-<a href="https://forms.gle/seTs2W87QmFvXMPX8" target="_blank" rel="noopener">
-    Apply Now
-</a>// Applications are handled by the linked Google Form.
+// Applications are handled by the linked Google Form.
